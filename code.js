46ab9e71989ccae1875e74c555c45ee7a9787341
Paragioudakis Antonis <antonisparagioudakis@gmail.com>
2021-09-12 14:56:26 +0300

    Add option to run from input xml

--- code.js
+++ code.js
@@ -605,7 +605,12 @@ Code.debugJS = function() {
     }
   };
   //var code = Blockly.JavaScript.workspaceToCode(Code.workspace);
-  var xml = Blockly.Xml.workspaceToDom(Code.workspace);
+  var xml = window.prompt("Give program representation (Empty to use blockly)");
+  if (xml == "")
+    xml = Blockly.Xml.workspaceToDom(Code.workspace);
+  else
+    xml = jQuery.parseXML(xml)
+  console.log(xml)
   xml = new XMLSerializer().serializeToString(xml);
 
   //var code = 'alert("Hello World!")';
